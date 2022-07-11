3968ff08622e26c77a6cb258ee659c476371b759
xuexue1112 <88174228+xuexue1112@users.noreply.github.com>
2022-07-10 17:21:10 -0700

    Initial setup

---
+++ starterCode/main.js
@@ -0,0 +1,13 @@
+/*
+ * File Name: main.js
+ * Description:
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+const IOhandler = require("./IOhandler"),
+  zipFilePath = `${__dirname}/myfile.zip`,
+  pathUnzipped = `${__dirname}/unzipped`,
+  pathProcessed = `${__dirname}/grayscaled`;
